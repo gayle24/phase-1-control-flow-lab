78908c35505221a758aa50a06b2c78e509d15f1d
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-14 20:37:59 +0300

    control flow lab completed

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let cost
+  if (distance <= 400) {
+    cost = "This one is on me!";
+  }  
+  else if (distance > 2000 && distance <= 2500) {
+    cost = "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500) {
+    cost = "No can do.";
+  }
+  return cost
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  return (destination="NYC" ? 'Ok, sounds good.' : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let grat
+  switch(tip) {
+    case "generous" : grat = "Thank you so much.";
+    break;
+    case "not as generous" : grat = "Thank you.";
+    break;
+    default : grat = "Bye.";
+  } 
+  return grat
 }
+
